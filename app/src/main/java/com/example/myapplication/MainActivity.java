a35dc6e10a108a0b0c11b8b45ca5106e398ada12
vicente_maroto_ESTECH <vicente.maroto@escuelaestech.es>
2020-02-17 14:20:45 +0100

    improved URL handling and basic reload support (still lacks of stop implementation)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,62 +2,89 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
 import android.os.Bundle;
-import android.os.Debug;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public WebView webView;
-
+    public Button reloadButton;
     public EditText url;
+    public boolean isWebviewLoading=true;
 
+    public void updateWebviewLoading(){
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView=findViewById(R.id.webview);
-        url=findViewById(R.id.urlBox);
 
+        //WEBVIEW
+        webView=findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        //RELOAD BUTTON
+        reloadButton=findViewById(R.id.reload);
+        reloadButton.setOnClickListener(new View.OnClickListener() {//STUB IMPLEMENTATION
+            @Override
+            public void onClick(View v) {
+                webView.reload();
+            }
+        });
+
+        //URL BOX CODE
+        url=findViewById(R.id.urlBox);
         url.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId== EditorInfo.IME_ACTION_DONE){
-                    updatewebviewContent(url.getText().toString());
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {//Input action listener
+                if(actionId== EditorInfo.IME_ACTION_DONE){//Checks for IME_ACTION_DONE
+                    updatewebviewContent(url.getText().toString());// Updates Webview on IME DONE
                 }
                 return false;
             }
         });
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
         updatewebviewContent("google.com");
+    }
+
+    public void updatewebviewContent(String url){
+        if(isValidURL(url)){
+            url=httpsStrict(url);
+            Log.v("WebViewDebug","url:" + url);
+            webView.loadUrl(url);
+        }
 
     }
-    public void updatewebviewContent(String url){
-        if(url==null){
-            Log.e("AppWebview","URL is NULL");
-            url="";
-        }else if(url.isEmpty()){
-            Log.d("AppWebview","URL is empty");
-        }else if(!url.startsWith("https://")) {
-            if (url.startsWith("http://")) {
-                String subUrl = url.substring(6);
-                //Log.v("AppVars", "subURL:" + subUrl);//DEBUG INFO
-                url = "http://" + subUrl;
-            } else {
-                url = "https://" + url;
-            }
-        }
-        Log.v("AppVars","url:"+url);
-        webView.loadUrl(url);
+    public boolean isValidURL(String url){
+        boolean validUrl=url.contains(".") && url!=null && !url.isEmpty();
+        Log.v("ValidURL","isValid:"+ validUrl);
+        return validUrl;
     }
+
+    //!(isHttp(url)|url.startsWith("https://")|url.startsWith("ftp://")
+    public String httpsStrict(String url){
+        if(isHttp(url)){
+            String subUrl = url.substring(6);
+            return  "https://" + subUrl;
+        }else{
+            return "https://" + url;
+        }
+    }
+    public boolean isHttp(String url){
+        boolean http=url.startsWith("http://");
+        Log.v("isHTTP","isHTTP:"+http);
+        return http;
+    }
+
     public void  updateWebviewContentCustom(String message){
         String customHtml = "<html><body><h1>UPS</h1><h2>" + message + "</h2></body></html>";
         webView.loadData(customHtml, "text/html", "UTF-8");
