564de25ab7f477465bbe7007e626be8349f3e7e1
vicente_maroto_ESTECH <vicente.maroto@escuelaestech.es>
2020-02-18 10:31:24 +0100

    Webview features 90% implemented. Unimplemented: goBack,goForward,stop, and dynamic reload/stop drawable

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,73 +12,116 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public WebView webView;
     public Button reloadButton;
-    public EditText url;
-    public boolean isWebviewLoading=true;
-
-    public void updateWebviewLoading(){
-
-    }
+    public EditText urlText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("WebViewVer","WebView Version:");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //WEBVIEW
         webView=findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebClient());
 
         //RELOAD BUTTON
         reloadButton=findViewById(R.id.reload);
         reloadButton.setOnClickListener(new View.OnClickListener() {//STUB IMPLEMENTATION
             @Override
             public void onClick(View v) {
+                Log.v("Reload","Reloading URL:" + webView.getOriginalUrl());
                 webView.reload();
             }
         });
 
         //URL BOX CODE
-        url=findViewById(R.id.urlBox);
-        url.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        urlText=findViewById(R.id.urlBox);
+        urlText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {//Input action listener
                 if(actionId== EditorInfo.IME_ACTION_DONE){//Checks for IME_ACTION_DONE
-                    updatewebviewContent(url.getText().toString());// Updates Webview on IME DONE
+                    updatewebviewContent(urlText.getText().toString());// Updates Webview on IME DONE
                 }
                 return false;
+            }
+        });
+        urlText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    urlText.setText(webView.getOriginalUrl());
+                    urlText.selectAll();
+                }else{
+                    setTittle();
+                }
             }
         });
         updatewebviewContent("google.com");
     }
 
+
+    //BACK BUTTON
+    //Default 0
+    // One Click to exit 1
+    public int exitCount=0;
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else if(exitCount==0){
+            Toast.makeText(MainActivity.this,"Press again to exit",
+                    Toast.LENGTH_LONG).show();
+            exitCount++;
+        }else if(exitCount>=1){
+            finish();
+        }
+    }
+
     public void updatewebviewContent(String url){
-        if(isValidURL(url)){
+        exitCount=0;//Restarts exit count
+        urlText.clearFocus();//Ensures that loses focus the url box
+        if(isValidURL(url)){//Loads specific URL
             url=httpsStrict(url);
             Log.v("WebViewDebug","url:" + url);
             webView.loadUrl(url);
+        }else{//Google Search in case that no url is provided
+            Log.v("WebViewDebug","Making Google Search");
+            url="https://www.google.com/search?q=" + url.replace(" ","+");
+            webView.loadUrl(url);
+            Log.d("URL","URL Loaded:" + webView.getOriginalUrl());
         }
-
     }
     public boolean isValidURL(String url){
-        boolean validUrl=url.contains(".") && url!=null && !url.isEmpty();
+        boolean validUrl=url.contains(".") && url!=null && !url.isEmpty()|hasSpaces(url);
         Log.v("ValidURL","isValid:"+ validUrl);
         return validUrl;
     }
+    public void setTittle(){
+        Log.v("TittleUpdate","Setting Tittle to " + webView.getTitle() + " from URL " + webView.getOriginalUrl());
+        urlText.setText(webView.getTitle());
+    }
+    boolean hasSpaces(String url){
+        boolean ret=url.contains(" ");
+        Log.d("HasSpaces","HasSpaces:"+ ret);
+        return  ret;
+    }
 
-    //!(isHttp(url)|url.startsWith("https://")|url.startsWith("ftp://")
     public String httpsStrict(String url){
         if(isHttp(url)){
             String subUrl = url.substring(6);
             return  "https://" + subUrl;
         }else{
+
             return "https://" + url;
         }
     }
+
     public boolean isHttp(String url){
         boolean http=url.startsWith("http://");
         Log.v("isHTTP","isHTTP:"+http);
@@ -88,5 +131,12 @@ public class MainActivity extends AppCompatActivity {
     public void  updateWebviewContentCustom(String message){
         String customHtml = "<html><body><h1>UPS</h1><h2>" + message + "</h2></body></html>";
         webView.loadData(customHtml, "text/html", "UTF-8");
+    }
+    class WebClient extends WebViewClient{
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            setTittle();
+        }
     }
 }
